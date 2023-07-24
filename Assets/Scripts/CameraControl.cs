e4d3af6b08bf4f98e66e42a89143ad67136a312a
Avro28 <65024986+Avro28@users.noreply.github.com>
2023-07-25 01:02:11 +0600

    camera movement enabled

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private float rotationSpeed = 500.0f;
+    private Vector3 mouseWorldPosStart;
+    private float zoomScale = 10.0f;
+    private float zoomMin = 0.1f;
+    private float zoomMax = 10.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.Mouse0))
+        {
+            CamOrbit();
+        }
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftAlt))
+        {
+            mouseWorldPosStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0) && !Input.GetKey(KeyCode.LeftAlt))
+        {
+            Pan();
+        }
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        
+    }
+
+    private void CamOrbit()
+    {
+        if (Input.GetAxis("Mouse Y")!=0 || Input.GetAxis("Mouse X") != 0)
+        {
+            float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            if (verticalInput<0 || (verticalInput > 0 && Camera.main.transform.position.y > 6.5))
+            {
+                transform.Rotate(Vector3.right, -verticalInput);
+            }
+            transform.Rotate(Vector3.up, horizontalInput, Space.World);
+        }
+    }
+    private void Pan()
+    {
+        if (Input.GetAxis("Mouse Y")!=0 || Input.GetAxis("Mouse X") != 0)
+        {
+            Vector3 mouseWorldPosDiff = mouseWorldPosStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position += mouseWorldPosDiff;
+        }
+    }
+    private void Zoom(float zoomDiff)
+    {
+        if (zoomDiff != 0)
+        {
+            mouseWorldPosStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+            Vector3 mouseWorldPosDiff = mouseWorldPosStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position += mouseWorldPosDiff;
+        }
+    }
+}
