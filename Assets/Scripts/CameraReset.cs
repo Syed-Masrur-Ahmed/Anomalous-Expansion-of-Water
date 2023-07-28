cb016a560dfbc900531422d3aa70c536ce90441c
Avro28 <65024986+Avro28@users.noreply.github.com>
2023-07-28 22:18:23 +0600

    Button to reset view has been added.

---
+++ Assets/Scripts/CameraReset.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CameraReset : MonoBehaviour
+{
+    public GameObject cam;
+    public void CamReset()
+    {
+        cam.transform.position = new Vector3(-1.0f, 0.0f, 0.0f);
+        cam.transform.rotation = Quaternion.Euler (new Vector3(0.0f, 25.0f, 0.0f));
+        Camera.main.orthographicSize = 5;
+    }
+}
