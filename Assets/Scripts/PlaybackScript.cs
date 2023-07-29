dfbe03b5aeefd02cace0358c923710b2b7361bab
Syed-Masrur-Ahmed <98674956+Syed-Masrur-Ahmed@users.noreply.github.com>
2023-07-29 16:13:41 +0600

    Polished playback without the frame skips

--- Assets/Scripts/PlaybackScript.cs
+++ Assets/Scripts/PlaybackScript.cs
@@ -1,30 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlaybackScript : MonoBehaviour
 {
-    public GameObject PauseButton;
-    public GameObject PlayButton;
+    public TMP_Text TimeScaleText;
+    public Button PauseButton;
+    public Button PlayButton;
+    public Button FastForwardButton;
+    public Button SlowDownButton;
+    private float[] timeScales = {0.5f, 0.75f, 1f, 1.5f, 2f};
+    private int timeIndex = 2;
+
+    public void ChangeTimeIndex(int n) 
+    {
+        timeIndex += n;
+        Time.timeScale = timeScales[timeIndex];
+        TimeScaleText.text = "Time: " + timeScales[timeIndex].ToString() + "x"; 
+        FastForwardButton.interactable = Convert.ToBoolean(4 - timeIndex);
+        SlowDownButton.interactable = Convert.ToBoolean(timeIndex);
+    }
+
+    public void ShowPlayButton() 
+    {
+        PauseButton.gameObject.SetActive(false);
+        PlayButton.gameObject.SetActive(true);
+    }
+
+    public void ShowPauseButton() 
+    {
+        PauseButton.gameObject.SetActive(true);
+        PlayButton.gameObject.SetActive(false);
+    }
     
     public void Pause() 
     {
         Time.timeScale = 0;
-        PauseButton.SetActive(false);
-        PlayButton.SetActive(true);
-    }
+        ShowPlayButton();
+    }    
 
     public void Play()
     {
-        Time.timeScale = 1;
-        PauseButton.SetActive(true);
-        PlayButton.SetActive(false);
+        ChangeTimeIndex(0);
+        ShowPauseButton();
     }
 
     public void FastForward() 
-    {
-        Time.timeScale = 1.5f;
-        PauseButton.SetActive(true);
-        PlayButton.SetActive(false);
+    {   
+        ChangeTimeIndex(1);
+        ShowPauseButton();
+    }
+
+    public void SlowDown() 
+    {   
+        ChangeTimeIndex(-1);
+        ShowPauseButton();
     }
 }
