c6f1b59c08843b46f56de320df19c2174a362280
Syed-Masrur-Ahmed <98674956+Syed-Masrur-Ahmed@users.noreply.github.com>
2023-08-12 01:26:55 +0600

    Fish bug fixes

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -7,15 +7,19 @@ public class FishMovement : MonoBehaviour
     private Rigidbody rb;
     private float speed = 1f;
     
-    private Vector3 GetNewVelocity() {
-        Vector3 newDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+    private Vector3 GetNewVelocity() 
+    {
+        float currentPosX = transform.position.x;
+        Vector3 newDirection = new Vector3(Random.Range(-1f, 1f), 0, 0);
+        if (currentPosX < 0.5 || currentPosX > 4.5) newDirection.x = 2.5f - currentPosX;
         if (newDirection.x > 0) transform.eulerAngles = new Vector3(0, 0, 0);
         if (newDirection.x < 0) transform.eulerAngles = new Vector3(0, 180, 0);
         return (newDirection / newDirection.magnitude) * speed;
     }
 
-    void FixedUpdate() {
-        if (Random.Range(0f, 1f) < 0.01f) rb.velocity = GetNewVelocity();
+    void FixedUpdate() 
+    {
+        if (Random.Range(0f, 1f) < 0.02f) rb.velocity = GetNewVelocity();
     }
 
     void Start()
